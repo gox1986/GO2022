9619ee891a1ab3ffbdcca10aaa6b784ca0ea8a0e
gox1986 <goran.milovanovic.20@vipos.edu.rs>
2022-11-13 19:14:20 +0100

    StableChang1

--- clishe/Assets/gameMan.cs
+++ clishe/Assets/gameMan.cs
@@ -10,10 +10,11 @@ public class gameMan : MonoBehaviour
   public Transform player;
   public int coins;
   public TextMeshProUGUI text;
+  public GameObject gameObjectt;
 
   private void Start()
   {
-    text.text = "Coins: 0";
+    text.text = "";
   }
 
   private void Update()
@@ -27,6 +28,31 @@ public class gameMan : MonoBehaviour
   public void AddCoins()
   {
     coins++;
-    text.text = "Coins: " +coins.ToString();
+    if (coins == 1)
+    {
+      gameObjectt.SetActive(true);
+      text.text = "Always try to be good person\n and use W-A-S-D to move";
+      StartCoroutine(wait());
+    }
+  
+    if (coins == 2)
+    {
+      gameObjectt.SetActive(true);
+      text.text = "If you are feeling down\n grab to anything you can. Jump to space";
+      StartCoroutine(wait());
+    }
+    if (coins == 3)
+    {
+      gameObjectt.SetActive(true);
+      text.text = "Admit when you are wrong\n Don't slide trough lies. Like you can on walls";
+      StartCoroutine(wait());
+    }
+  }
+
+  IEnumerator wait()  
+  {
+    yield return new WaitForSeconds(10);
+    text.text = "";
+    gameObjectt.SetActive(false);
   }
 }
